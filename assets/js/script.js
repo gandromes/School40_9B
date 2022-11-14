153f9b280d4c066ae0293e68fcb18ba2a658b35f
Roma Romanovv <moto.pers2007@gmail.com>
2022-11-14 23:30:28 +0400

    defer

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,148 +11,146 @@ swithTopic.addEventListener(
   { passive: true }
 )
 
-window.addEventListener('DOMContentLoaded', function () {
-  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
-  const body = document.querySelector('body')
-  const intro = document.querySelector('#intro')
-  const header = document.querySelector('#header')
-  const navToggle = document.querySelector('#navToggle')
-  const nav = document.querySelector('#nav')
-  
-  let headerH = header.offsetHeight + 10
-  let introH = intro.offsetHeight
-  let scroll = window.pageYOffset
+const lockPaddingValue =
+  window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
+const body = document.querySelector('body')
+const intro = document.querySelector('#intro')
+const header = document.querySelector('#header')
+const navToggle = document.querySelector('#navToggle')
+const nav = document.querySelector('#nav')
 
-  //Fixed Header
-  window.onscroll = () => {
-    scroll = window.pageYOffset
-    introH = intro.offsetHeight
-    headerH = header.offsetHeight
-    scrollCheker(introH + headerH + 22, scroll - headerH)
-  }
+let headerH = header.offsetHeight + 10
+let introH = intro.offsetHeight
+let scroll = window.pageYOffset
 
-  //Smooth Scroll
-  nav.onclick = (e) => {
-    const dataScroll = e.path[0].dataset.scroll
-    if (dataScroll) {
-      e.preventDefault()
-      let blockOffSet = document.querySelector(dataScroll).offsetTop
-      nav.classList.remove('active')
+//Fixed Header
+window.onscroll = () => {
+  scroll = window.pageYOffset
+  introH = intro.offsetHeight
+  headerH = header.offsetHeight
+  scrollCheker(introH + headerH + 22, scroll - headerH)
+}
 
-      if (dataScroll == '#begin') {
-        window.scrollTo({
-          top: blockOffSet - headerH,
-          behavior: 'smooth',
-        })
-      } else {
-        window.scrollTo({
-          top: blockOffSet - headerH * 2,
-          behavior: 'smooth',
-        })
-      }
-    }
-  }
-
-  //NavToggle
-  navToggle.onclick = (event) => {
-    event.preventDefault()
-    nav.classList.toggle('active')
-  }
-
-  //Modal
-  const modalCall = document.querySelector('[data-modal]')
-  const modalClose = document.querySelector('[data-close]')
-  const modal = document.querySelector('.modal')
-
-  modalCall.addEventListener('click', (event) => {
-    event.preventDefault()
-    let modalId = document.querySelector(modalCall.dataset.modal)
-    modalId.showModal()
-
-    body.style.paddingRight = lockPaddingValue
-    header.className.includes('fixed') &&
-      (header.style.paddingRight = lockPaddingValue)
-
-    body.classList.add('no-scroll')
+//Smooth Scroll
+nav.onclick = (e) => {
+  const dataScroll = e.path[0].dataset.scroll
+  if (dataScroll) {
+    e.preventDefault()
+    let blockOffSet = document.querySelector(dataScroll).offsetTop
     nav.classList.remove('active')
 
-    setTimeout(() => {
-      modalId.childNodes[1].style.cssText = `transform: rotateX(0);`
-    }, 200)
-  })
-
-  modalClose.addEventListener('click', (event) => {
-    event.preventDefault()
-
-    let modalParent = modalClose.parentElement
-    modalParent.style.cssText = `transform: rotateX(90deg);`
-
-    setTimeout(() => {
-      body.style.paddingRight = '0px'
-      header.className.includes('fixed') && (header.style.paddingRight = '0px')
-      modalParent.parentElement.close()
-      body.classList.remove('no-scroll')
-    }, 200)
-  })
-
-  modal.onclick = (event) => {
-    if (!(event.path[0] == modal)) return false
-
-    const modalDialog = modal.firstElementChild
-    modalDialog.style.cssText = `transform: rotateX(90deg)`
-
-    setTimeout(() => {
-      body.style.paddingRight = '0px'
-      header.className.includes('fixed') && (header.style.paddingRight = '0px')
-      modal.close()
-      body.classList.remove('no-scroll')
-    }, 200)
-  }
-
-  //Slider - swiper
-  new Swiper('.fullwe', {
-    navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next',
-    },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-    },
-    autoHeight: true,
-    watchOverflow: true,
-    loop: true,
-    autoplay: {
-      delay: 25000,
-      disableOnIteraction: true,
-    },
-    speed: 800,
-    touchRatio: 0.8,
-    touchAngle: 50,
-
-    preloadImages: false,
-    lazy: {
-      loadOnTransitionStart: true,
-      loadPrevNext: true,
-    },
-
-    a11y: {
-      enabled: true,
-      prevSlideMessage: 'Вернуться к прошлому слайду',
-      nextSlideMessage: 'Показать следующий слайд',
-      firstSlideMessage: 'Это первый слайд',
-      lastSlideMessage: 'Это последний слайд',
-      paginationBulletMessage: 'Перейти к слайду {{index}}',
-      itemRoleDescriptionMessage: 'Следующие фото в слайдере',
-    },
-  })
-
-  function scrollCheker(introH, scroll) {
-    if (scroll > introH) {
-      header.classList.add('fixed')
+    if (dataScroll == '#begin') {
+      window.scrollTo({
+        top: blockOffSet - headerH,
+        behavior: 'smooth',
+      })
     } else {
-      header.classList.remove('fixed')
+      window.scrollTo({
+        top: blockOffSet - headerH * 2,
+        behavior: 'smooth',
+      })
     }
   }
+}
+
+//NavToggle
+navToggle.onclick = (event) => {
+  event.preventDefault()
+  nav.classList.toggle('active')
+}
+
+//Modal
+const modalCall = document.querySelector('[data-modal]')
+const modalClose = document.querySelector('[data-close]')
+const modal = document.querySelector('.modal')
+
+modalCall.addEventListener('click', (event) => {
+  event.preventDefault()
+  let modalId = document.querySelector(modalCall.dataset.modal)
+  modalId.showModal()
+
+  body.style.paddingRight = lockPaddingValue
+  header.className.includes('fixed') &&
+    (header.style.paddingRight = lockPaddingValue)
+
+  body.classList.add('no-scroll')
+  nav.classList.remove('active')
+
+  setTimeout(() => {
+    modalId.childNodes[1].style.cssText = `transform: rotateX(0);`
+  }, 200)
 })
 
+modalClose.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  let modalParent = modalClose.parentElement
+  modalParent.style.cssText = `transform: rotateX(90deg);`
+
+  setTimeout(() => {
+    body.style.paddingRight = '0px'
+    header.className.includes('fixed') && (header.style.paddingRight = '0px')
+    modalParent.parentElement.close()
+    body.classList.remove('no-scroll')
+  }, 200)
+})
+
+modal.onclick = (event) => {
+  if (!(event.path[0] == modal)) return false
+
+  const modalDialog = modal.firstElementChild
+  modalDialog.style.cssText = `transform: rotateX(90deg)`
+
+  setTimeout(() => {
+    body.style.paddingRight = '0px'
+    header.className.includes('fixed') && (header.style.paddingRight = '0px')
+    modal.close()
+    body.classList.remove('no-scroll')
+  }, 200)
+}
+
+//Slider - swiper
+new Swiper('.fullwe', {
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  autoHeight: true,
+  watchOverflow: true,
+  loop: true,
+  autoplay: {
+    delay: 25000,
+    disableOnIteraction: true,
+  },
+  speed: 800,
+  touchRatio: 0.8,
+  touchAngle: 50,
+
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: true,
+    loadPrevNext: true,
+  },
+
+  a11y: {
+    enabled: true,
+    prevSlideMessage: 'Вернуться к прошлому слайду',
+    nextSlideMessage: 'Показать следующий слайд',
+    firstSlideMessage: 'Это первый слайд',
+    lastSlideMessage: 'Это последний слайд',
+    paginationBulletMessage: 'Перейти к слайду {{index}}',
+    itemRoleDescriptionMessage: 'Следующие фото в слайдере',
+  },
+})
+
+function scrollCheker(introH, scroll) {
+  if (scroll > introH) {
+    header.classList.add('fixed')
+  } else {
+    header.classList.remove('fixed')
+  }
+}
